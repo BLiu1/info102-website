db8ae530cab07a1f8dccae601ec119ccd56c0842
Brian Liu <liubrian1998@yahoo.com>
2016-11-16 02:37:23 -0800

    Added licensing info

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -6,9 +6,9 @@ $('a[href*=#]:not([href=#])').click(function() {
 
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
             }, 500);
             return false;
         }
